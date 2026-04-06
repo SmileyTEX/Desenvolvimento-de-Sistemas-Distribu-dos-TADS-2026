a4050779ccb99890e4dbbd7b56c8aca58661c4ca
Samuel Oliveira <161156540+SmileyTEX@users.noreply.github.com>
2026-04-06 17:45:36 -0300

    Add files via upload

---
+++ biblioteca/biblioteca/livros/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers 
+from rest_framework.reverse import reverse 
+from .models import Autor, Livro 
+class AutorSerializer(serializers.ModelSerializer): 
+    _links = serializers.SerializerMethodField() 
+    class Meta: 
+        model = Autor 
+        fields = ['id', 'nome', 'nacionalidade', '_links'] 
+    def get__links(self, obj): 
+        request = self.context.get('request') 
+        links = { 
+            "self": reverse('autor-detail', args=[obj.pk], request=request), 
+            "livros": reverse('livro-list', request=request) + f"?autor={obj.pk}" 
+        } 
+        user = request.user if request else None 
+        if user and user.is_authenticated and user.has_perm('livros.change_autor'): 
+            links['editar'] = reverse('autor-detail', args=[obj.pk], request=request) 
+            links['excluir'] = reverse('autor-detail', args=[obj.pk], request=request) 
+        return links 
+ 
+class LivroSerializer(serializers.ModelSerializer): 
+    _links = serializers.SerializerMethodField() 
+    autor = AutorSerializer(read_only=True) 
+    class Meta: 
+        model = Livro 
+        fields = ['id', 'titulo', 'ano_publicacao', 'autor', '_links'] 
+    def get__links(self, obj): 
+        request = self.context.get('request') 
+        return { 
+            "self": reverse('livro-detail', args=[obj.pk], request=request), 
+            "autor": reverse('autor-detail', args=[obj.autor.pk], request=request) 
+        }
